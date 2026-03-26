6ce63476fea4cd453c7f774ea8c3cb9c1305fc23
Ariela Molina <arielamolina0595@gmail.com>
2026-03-25 18:41:22 -0600

    segundo avance

--- src/gestion_hospital/ArbolBinario.java
+++ src/gestion_hospital/ArbolBinario.java
@@ -13,19 +13,45 @@ public class ArbolBinario {
         if(actual == null){
             return new Nodo(paciente);
         }
-        
         if(paciente.getId() < actual.dato.getId()){
             actual.izquierda = insertarRecursivo(actual.izquierda, paciente);
-        }else{
-            if(paciente.getId() > actual.dato.getId()){
+        }else if(paciente.getId() > actual.dato.getId()){
                 actual.derecha = insertarRecursivo(actual.derecha, paciente);
-            }
         }
         return actual;
     }
     
-    public Nodo insertar(Nodo actual, Paciente p){
-        return insertarRecursivo(actual, p);
+    public Nodo insertar(Paciente p){
+        return insertarRecursivo(this.raiz, p);
     }
     
+    private Nodo buscarId(Nodo actual, int id) {
+        while (actual != null) {
+            if (id == actual.dato.getId()) {
+                return actual; 
+            }
+            if (id < actual.dato.getId()) {
+                actual = actual.izquierda; 
+            } else {
+                actual = actual.derecha; 
+            }
+        }
+        return null; 
+    }
+    
+    public Nodo buscar(int id) {
+        return buscarId(this.raiz, id);
+    }
+    
+    private void ordenarId(Nodo actual){
+        if(actual != null){
+            ordenarId(actual.izquierda);
+            System.out.println(actual.dato.toString());
+            ordenarId(actual.derecha);
+        }
+    }
+    
+    public void Pa_Ordenados(){
+        ordenarId(this.raiz);
+    }
 }
