229d6b87939ade434b315a6415c39175a0a458a8
Ariela Molina <arielamolina0595@gmail.com>
2026-04-05 01:14:53 -0600

    tercer avance

--- src/gestion_hospital/ArbolBinario.java
+++ src/gestion_hospital/ArbolBinario.java
@@ -21,8 +21,8 @@ public class ArbolBinario {
         return actual;
     }
     
-    public Nodo insertar(Paciente p){
-        return insertarRecursivo(this.raiz, p);
+    public void insertar(Paciente p){
+        this.raiz = insertarRecursivo(this.raiz, p);
     }
     
     private Nodo buscarId(Nodo actual, int id) {
@@ -39,19 +39,86 @@ public class ArbolBinario {
         return null; 
     }
     
-    public Nodo buscar(int id) {
-        return buscarId(this.raiz, id);
+    public Paciente buscarPaciente(int id) {
+        Nodo resultado = buscarId(this.raiz, id);
+        if (resultado != null) {
+            return resultado.dato; 
+        }
+        return null; 
     }
     
-    private void ordenarId(Nodo actual){
+    //Pacientes ordenados de menor a mayor por ID
+    //(Izquierda -> Raíz -> Derecha)
+    //Reportes ordenados
+    private void in_Orden (Nodo actual){
         if(actual != null){
-            ordenarId(actual.izquierda);
+            in_Orden(actual.izquierda);
             System.out.println(actual.dato.toString());
-            ordenarId(actual.derecha);
+            in_Orden(actual.derecha);
+        }
+    } 
+    
+    //Anota primero al jefe (la raíz) y luego a los hijos
+    //(Raíz -> Izquierda -> Derecha)
+    //Para respaldar o clonar el arbol
+    private void pre_Orden(Nodo actual){
+        if(actual != null){
+           System.out.println(actual.dato.toString());
+           pre_Orden(actual.izquierda);
+           pre_Orden(actual.derecha);           
         }
     }
     
-    public void Pa_Ordenados(){
-        ordenarId(this.raiz);
+    //Visita primero todos los hijos (las hojas) y de último deja a la raíz
+    //(Izquierda -> Derecha -> Raíz)
+    //Para borrar o liberar memoria
+    private void post_Orden(Nodo actual){
+        if (actual != null) {
+            post_Orden(actual.izquierda);
+            post_Orden(actual.derecha);
+            System.out.println(actual.dato.toString());
+        }
+    }
+    
+    public void mostrarInOrden(){
+        in_Orden(this.raiz);
+    }
+    
+    public void mostrarPreOrden() {
+        pre_Orden(this.raiz);
+    }
+
+    public void mostrarPostOrden() {
+        post_Orden(this.raiz);
+    }
+    
+    private Nodo eliminar_Paciente(Nodo actual, int id) {
+        if (actual == null) return null;
+
+        if (id < actual.dato.getId()) {
+            actual.izquierda = eliminar_Paciente(actual.izquierda, id);
+        } else 
+            if (id > actual.dato.getId()) {
+            actual.derecha = eliminar_Paciente(actual.derecha, id);
+            } else { 
+                if (actual.izquierda == null) return actual.derecha;
+                if (actual.derecha == null) return actual.izquierda;
+
+                actual.dato = obtener_MasPequeno(actual.derecha);
+                actual.derecha = eliminar_Paciente(actual.derecha, actual.dato.getId());
+            }
+        return actual;
+    }       
+    
+    private Paciente obtener_MasPequeno(Nodo raiz){
+        if(raiz.izquierda == null){
+            return raiz.dato;
+        }else{
+            return obtener_MasPequeno(raiz.izquierda);
+        }
+    }
+    
+    public void eliminar(int id){
+        this.raiz = eliminar_Paciente(this.raiz, id);
     }
 }
